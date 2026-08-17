e5e5134008998591f2f0ba3729d2609d722e2c98
pahasara-ridmaka <pahasara.ridmaka95@gmail.com>
2026-08-17 23:42:59 +0530

    fix: edge

--- app/blog/[slug]/opengraph-image.tsx
+++ app/blog/[slug]/opengraph-image.tsx
@@ -1,7 +1,6 @@
 import { ImageResponse } from "next/og";
 import { getPostBySlug } from "@/lib/blog";
 
-export const runtime = "edge";
 export const alt = "Blog post cover image";
 export const size = { width: 1200, height: 630 };
 export const contentType = "image/png";
